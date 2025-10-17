4214a6e33eca17dee12c1de0c6f8aaf356e09308
venkatasai2801 <saisanju2801@gmail.com>
2025-10-17 16:05:04 +0530

    Add files via upload

---
+++ evenodd.java
@@ -0,0 +1,19 @@
+package com.codegnanassignment;
+
+import java.util.Scanner;
+
+public class evenodd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner scanner = new Scanner(System.in);
+		int num = scanner.nextInt();
+		if (num % 2 ==0);
+		System.out.println("Even");
+		System.out.println("Odd");
+		scanner.close();
+		
+	}	
+
+}
