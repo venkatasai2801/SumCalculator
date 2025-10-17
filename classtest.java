ad533862eb50a941c6a766e973ddf7a15d7aacac
venkatasai2801 <saisanju2801@gmail.com>
2025-10-17 16:10:49 +0530

    Add files via upload

---
+++ classtest.java
@@ -0,0 +1,17 @@
+package com.codegnanassignment;
+
+import java.util.Scanner;
+
+public class classtest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Scanner scanner = new Scanner(System.in);
+		 
+		 char ch = scanner.next(). charAt(0);
+		 int asciiValue = (int) ch ;
+		 System.out.println(asciiValue);
+
+	}
+
+}
